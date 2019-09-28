f7eeea6e986887bc6e8dfa90167d226316a1baca
Jacob <jlap4567@gmail.com>
2019-09-28 06:18:30 -0400

    created lv2 screen

--- wethepeople/src/pages/Lv2/Level2.js
+++ wethepeople/src/pages/Lv2/Level2.js
@@ -1,8 +1,8 @@
 import React, { Component }  from "react";
-import {Button} from "react-bootstrap";
+import {Button, Form, Col, Row, Container} from "react-bootstrap";
 import styled from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reseach1 from "../InterActiveRoadMap/assets/research_l2.png";
+import reseach2 from "../InterActiveRoadMap/assets/research_l2.png";
 
 import { Link } from 'react-router-dom';
 
@@ -15,16 +15,76 @@ import { Link } from 'react-router-dom';
 export class Level2 extends Component{
     render() {
         return (
-            <div>
-                <img src = {reseach1}></img>
-                <p>Discover Your Voice And Find Your Passion</p>
+            <Container>
+                <img src = {reseach2}></img>
+                <p>Research The Legeslative Landscape in Your State</p>
                 <p>
-                    Congrats! Just By Starting here, you are on the path for success!
+                    For this section you will need to some local laws pertaining you your topic.
                 </p>
+                <p>
+                    Lets start with 3! (Enter the link to the law's website in the text-box)
+                </p>
+
+                <Form>
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                        Law 1
+                        </Form.Label>
+                        <Col sm="10">
+                        <Form.Control type="URL" placeholder="URL" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                        Notes
+                        </Form.Label>
+                        <Col sm="10">
+                        <Form.Control as="textarea" rows="3" />
+                        </Col>
+                    </Form.Group>
+                    
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                        Law 2
+                        </Form.Label>
+                        <Col sm="10">
+                        <Form.Control type="URL" placeholder="URL" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                        Notes
+                        </Form.Label>
+                        <Col sm="10">
+                        <Form.Control as="textarea" rows="3" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                        Law 3
+                        </Form.Label>
+                        <Col sm="10">
+                        <Form.Control type="URL" placeholder="URL" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                        <Form.Label column sm="2">
+                        Notes
+                        </Form.Label>
+                        <Col sm="10">
+                        <Form.Control as="textarea" rows="3" />
+                        </Col>
+                    </Form.Group>
+                </Form>
                 <Link to='/InterActiveRoadMap3'>
-                    <Button bright="light">Next Level!</Button>
+                    <Button bright="light">Submit!</Button>
                 </Link>
-            </div>
+            </Container>
         );
     }
 }
