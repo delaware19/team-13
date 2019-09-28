f960869bed921a536a54a479edd6bf28c3803489
schebruch <sac320@lehigh.edu>
2019-09-28 02:38:46 -0400

    fixed container padding for forms

--- wethepeople/src/pages/SignUp/SignUp.js
+++ wethepeople/src/pages/SignUp/SignUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 //import styled from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
@@ -13,58 +13,60 @@ import { Link } from 'react-router-dom';
 export class SignUp extends Component {
   render() {
     return (
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="firstName" placeholder="first name" />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="lastName" placeholder="last name" />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+      <Container>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="firstName" placeholder="first name" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="lastName" placeholder="last name" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
               </Form.Text>
-        </Form.Group>
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Re-enter Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Re-enter Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="state" placeholder="State" />
-        </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>State</Form.Label>
+            <Form.Control type="state" placeholder="State" />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Political Injustice</Form.Label>
-          <Form.Control type="password" placeholder="Injustice" />
-        </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Political Injustice</Form.Label>
+            <Form.Control type="password" placeholder="Injustice" />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="phoneNumber" placeholder="Number" />
-        </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="phoneNumber" placeholder="Number" />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
 
-        <Link to='/InterActiveRoadMap'>
+          <Link to='/InterActiveRoadMap'>
             <Button variant="primary" type="submit">
-             Submit
+              Submit
              </Button>
-            </Link>
-        
-      </Form>
+          </Link>
+
+        </Form>
+      </Container>
     );
   }
 }
