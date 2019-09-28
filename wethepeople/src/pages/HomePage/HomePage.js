3ff3cfe478b26109bf42b6272693a8e04ac809d0
Jacob <jlap4567@gmail.com>
2019-09-27 22:33:40 -0400

    created sign up form

--- wethepeople/src/pages/HomePage/HomePage.js
+++ wethepeople/src/pages/HomePage/HomePage.js
@@ -1,9 +1,7 @@
 import React, { Component }  from "react";
-import { TypeFormComponent } from "./components/TypeFormComponent";
-import styled from "styled-components";
-
-
-
+import {Form, Button} from "react-bootstrap";
+//import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // The overall page when you click on welcome page
 // Top Wrapper takes the elements on the top of the page
@@ -13,15 +11,51 @@ import styled from "styled-components";
 
 
 
-
-
-
 export class HomePage extends Component{
     render() {
         return (
-            <div>
-                <TypeFormComponent></TypeFormComponent>
-            </div>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Re-enter Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control type="state" placeholder="State" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Political Injustice</Form.Label>
+                    <Form.Control type="password" placeholder="Injustice" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Phone Number</Form.Label>
+                    <Form.Control type="phoneNumber" placeholder="Number" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+
+                
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         );
     }
 }
