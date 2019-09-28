6d25f0a5f17640e8ba674f83e9fb6383ce4e4a8b
Natalia Jacobo <njacobo@fulcrumgt.com>
2019-09-27 21:23:54 -0400

    Made pages and homepage and skeleton of future pages and components

---
+++ wethepeople/src/pages/HomePage/HomePage.js
@@ -0,0 +1,27 @@
+import React, { Component }  from "react";
+
+
+
+
+// The overall page when you click on welcome page
+// Top Wrapper takes the elements on the top of the page
+// Listing Wrapper takes care of the dynamically given items
+// and puts them in a grid that reponsively adjusts
+
+
+
+
+
+
+
+export class HomePage extends Component{
+    render() {
+        return (
+            <div>
+                hi there this is the home page
+            </div>
+        );
+    }
+}
+
+export default HomePage;
